688595d3befae9d92510da624ba7bda920383eb1
feldim2425 <feldim2425@gmail.com>
2019-11-08 00:27:36 +0100

    Song announce settings

--- src/frontend/screens/setting_panels/general.vue
+++ src/frontend/screens/setting_panels/general.vue
@@ -5,7 +5,13 @@
                 <b-form-radio :checked="settings.misc.playback" @change="updateSettings($event, path='misc.playback')" value="native">Serverside playback</b-form-radio>
                 <b-form-radio :checked="settings.misc.playback" @change="updateSettings($event, path='misc.playback')" value="voicechat">Discord voicechat</b-form-radio>
                 <b-form-radio :checked="settings.misc.playback" @change="updateSettings($event, path='misc.playback')" value="webstream">HTTP Audio stream</b-form-radio>
-                <span v-if="settings.misc.playback=='webstream'">HTTP Stream: <a :href="streamURL" rel="noopener noreferrer" target="_blank">{{streamURL}}</a></span>
+                <span v-if="settings.misc.playback=='webstream'">HTTP Stream: <a class="media-link" :href="streamURL" rel="noopener noreferrer" target="_blank">{{streamURL}}</a></span>
+            </b-form-group>
+        </b-card>
+        <b-card bg-variant="secondary">
+            <b-form-group label="Announce next song">
+                <b-form-checkbox :checked="settings.misc.discord_songAnnounce" @input="updateSettings($event, path='misc.discord_songAnnounce')">In Discord text channel</b-form-checkbox>
+                <b-form-checkbox :checked="settings.misc.twitch_songAnnounce" @input="updateSettings($event, path='misc.twitch_songAnnounce')">In Twitch channel</b-form-checkbox>
             </b-form-group>
         </b-card>
     </div>
@@ -27,7 +33,9 @@ export default class GeneralettingsPanel extends Mixins(CustomScopeMixin){
     get settings() : object{
         return Object.assign({            
             misc: {
-                playback: 'native'
+                playback: 'native',
+                discord_songAnnounce: false,
+                twitch_songAnnounce: false
             }
         }, this.value)
     }
@@ -42,3 +50,9 @@ export default class GeneralettingsPanel extends Mixins(CustomScopeMixin){
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    .media-link {
+        color: #1f1152
+    }
+</style>
