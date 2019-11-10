42eaa68087fa8f066a01c014b4c2114c8c25b944
feldim2425 <feldim2425@gmail.com>
2019-11-10 21:29:35 +0100

    Twitch service login

--- src/backend/services/twitch.ts
+++ src/backend/services/twitch.ts
@@ -1,44 +1,76 @@
 import { EventEmitter } from 'events';
-import {Client} from 'tmi.js'
+import tmi from 'tmi.js'
 import { AccountService } from './service_manager';
 import _ from 'lodash';
 
+type TwitchConfig = {
+    username: string,
+    oauth: string
+    channels: string[]
+}
 
 export default class TwitchService extends EventEmitter implements AccountService {
 
     private _enabled: boolean = false;
+    private _data: TwitchConfig = {username: undefined, oauth: undefined, channels: []}
     loggedin: boolean;
 
     public configure(config: object) {
-        const oauth = _.get(config, 'logins.twitch.oauth')
-        const username = _.get(config, 'logins.twitch.username')
+        let ndata: TwitchConfig = {
+            oauth: _.get(config, 'logins.twitch.oauth'),
+            username: _.get(config, 'logins.twitch.username'),
+            channels: []
+        }
+        let channel :string = _.get(config, 'inputs.twitch_channel')
+        if(!_.isEmpty(channel)){
+            if(channel.startsWith('#')){
+                channel = '#' + channel
+            }
+            ndata.channels.push(channel)
+        }
         let enabled = _.get(config, 'logins.twitch.enabled', false)
 
-        if(_.isEmpty(oauth) || _.isEmpty(username)){
+        if(_.isEmpty(ndata.oauth) || _.isEmpty(ndata.username) || _.isEmpty(ndata.channels)){
             enabled = false
         }
 
+        const forceReconnect = !_.isEqual(ndata, this._data)
+        this._data = ndata
+
         if(this._enabled !== enabled){
             this._enabled = enabled
-            if(enabled){
+            if(this._enabled){
                 this.connect()
             }
             else {
                 this.disconnect()
             }
         }
+        else if(forceReconnect && this._enabled){
+            this.disconnect()
+            this.connect()
+        }
     }
 
-    private _tmi : Client
+    private _tmi : tmi.Client
 
     public connect(): void{
-        console.debug('>> CONNECT')
-       // this._tmi = new Client({});
+        this._tmi = tmi.client({
+            identity: {
+                username: this._data.username,
+                password: this._data.oauth
+            },
+            channels: this._data.channels
+        });
+        this._tmi.on('connected', () => console.log(`Twitch service connected to [${this._data.channels.join(', ')}]`));
+        this._tmi.connect().catch((e)=> console.error(e))
     }
 
     public disconnect(): void{
-        console.debug('>> DISCONNECT')
-        // this._tmi = new Client({});
+        if(this._tmi === undefined){
+            return
+        }
+        this._tmi.disconnect()
     }
 
     public get enabled(): boolean{
